83153f8981389f800e1c17f49dde5508aefabe2e
Barrian <56634530+Barrian@users.noreply.github.com>
2019-11-01 12:01:33 +0000

    Add files via upload

--- lecture/slide.py
+++ lecture/slide.py
@@ -12,11 +12,20 @@ Purpose:
 import os
 import argparse
 parser=argparse.ArgumentParser()
-parser.add_argument("-w","-week",type=int, help="Parses l?.tex in ? folder, outputs l?.pdf")
+parser.add_argument("-w","-week",type=int,nargs="+", help="Parses l?.tex in ? folder, outputs l?.pdf")
+parser.add_argument("-t","-tar", help="Adds new file to tar file")
 args=parser.parse_args()
-if args.w>=1:
+if len(args.w)==1 and not args.t:
     week=args.w
-    os.chdir(week)
-    os.system('pdflatex l%d.tex'%(week))
+    os.chdir(str(week))
+    os.system('pdflatex l%s.tex'%(str(week)))
     os.chdir('..')
+elif len(args.w)==2 and not args.t:
+    print args.w[0]
+    print args.w[1]
+elif args.t and args.w:
+    tarFile=args.t#"slides.tar.gz"
+    print 'file: ',tarFile 
+    os.system('tar -cvzf %s */l*.pdf'%(tarFile))
+
 
